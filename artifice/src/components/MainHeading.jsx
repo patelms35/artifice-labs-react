545e59c670fa0807dc4d284780f0028b54d9f1e4
Aaditya-Dobariya1 <aadityadobariya@gmail.com>
2024-09-11 21:25:28 +0530

    minor changes

--- artifice/src/components/MainHeading.jsx
+++ artifice/src/components/MainHeading.jsx
@@ -1,10 +1,11 @@
-
-const MainHeading = ({ title, backgroundTitle}) => {
+const MainHeading = ({ title, backgroundTitle }) => {
   return (
     <div className="flex flex-col items-center relative" data-aos="fade-down">
       <div className="relative">
         {/* First About Us */}
-        <span className="text-6xl font-bold text-gray-200">{backgroundTitle}</span>
+        <span className="text-6xl font-bold text-gray-200">
+          {backgroundTitle}
+        </span>
         {/* Second About Us overlapping between the top and middle of the first */}
         <span className="text-4xl font-bold text-gray-800 absolute top-8 left-12">
           {title}
