1d52c7592b50908f50fed97924cac2492b745708
Aaditya-Dobariya1 <aadityadobariya@gmail.com>
2024-09-19 20:33:26 +0530

    main heading alignment

--- artifice/src/components/MainHeading.jsx
+++ artifice/src/components/MainHeading.jsx
@@ -2,16 +2,19 @@ const MainHeading = ({ title, backgroundTitle }) => {
   return (
     <div className="flex flex-col items-center relative" data-aos="fade-up">
       <div className="relative">
-        {/* First About Us */}
-        <span className="text-5xl md:text-6xl  font-bold text-gray-200">
+        {/* Background Title */}
+        <span className="text-3xl sm:text-5xl md:text-7xl lg:text-8xl font-bold text-gray-200">
           {backgroundTitle}
         </span>
-        {/* Second About Us overlapping between the top and middle of the first */}
-        <span className="text-3xl md:text-4xl font-bold text-gray-800 absolute top-8 left-12">
+
+        {/* Foreground Title */}
+        <span className="text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-gray-800 absolute top-4 sm:top-4 md:top-8 left-4 sm:left-8 md:left-12 lg:left-16 xl:left-24 2xl:left-20">
           {title}
         </span>
       </div>
-      <div className="h-1 w-16 bg-[#106eea] mt-3 ml-[5rem]"></div>
+
+      {/* Line below the title */}
+      <div className="h-1 w-16 bg-[#106eea] mt-2 sm:mt-3 md:mt-4 ml-[2rem] sm:ml-[3rem] md:ml-[5rem]"></div>
     </div>
   );
 };
