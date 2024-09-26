57393532a7e0d9da015280d59cbb6da3726fc7dc
Patel Mark <patelmark5330@gmail.com>
2024-09-26 15:37:24 +0530

    Now visible im mark laptop

--- artifice/src/Orbit/magic.jsx
+++ artifice/src/Orbit/magic.jsx
@@ -2,8 +2,8 @@ import OrbitingCircles from "./orbitingCircle.jsx";
 
 export function OrbitingCirclesDemo({ innerIcons =[], outerIcons = [] }) {
   return (
-    <div className="h-[400px] w-[500px] relative flex md:h-[500px] md:w-full flex-col items-center justify-center mt-5 md:mt-0 md:mr-32 2xl">
-      <span className="pointer-events-none bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-7xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
+    <div className="h-[400px] w-[500px] relative flex md:w-full flex-col items-center justify-center mt-5 md:mt-0 md:mr-32 2xl">
+      <span className="pointer-events-none bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-5xl md:text-7xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
         Technology
       </span>
 
--- artifice/src/Orbit/orbitingCircle.jsx
+++ artifice/src/Orbit/orbitingCircle.jsx
@@ -8,7 +8,6 @@ export default function OrbitingCircles({
   delay = 10,
   radius = 50,
   path = true,
-  icon
 }) {
   return (
     <>
@@ -19,7 +18,7 @@ export default function OrbitingCircles({
           className="pointer-events-none absolute inset-0 size-full"
         >
           <circle
-            className="stroke-black/10 stroke-1 dark:stroke-white/10"
+            className="stroke-black/10 stroke-1 "
             cx="50%"
             cy="50%"
             r={radius}
